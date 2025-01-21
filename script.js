c21fed704f1d18174ba409a12f4f97db613047d3
Nikhil Bhavani <86593547+nikhilbhavani@users.noreply.github.com>
2025-01-21 16:13:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -22,6 +22,77 @@ const Gameboard = (() => {
         return true;
     };
 
+    return{mark,getBoard};
+})();
+
+const Player = (name,symbol) => {
+    return{name,symbol};
+};
+
+const GameControl = (() => {
+    let players=[];
+    let currentplayer;
+    let gameend=false;
+
+    const assignplayer = (player1,player2)=>{
+        players=[player1,player2];
+        currentplayer=player1
+    };
+
+    const humanplayer1=document.getElementById("humanplayer1");
+    const humanplayer2=document.getElementById("humanplayer2");
+    const compplayer1=document.getElementById("compplayer1");
+    const compplayer2=document.getElementById("compplayer2");
+    const start=document.querySelector(".start");
+    const turntext=document.querySelector(".turn-text");
+    const GB=document.querySelector(".game-board");
+    const squares=document.querySelectorAll(".square")
     
+
+    let player1type;
+    let player2type;
+
+    humanplayer1.addEventListener('click', () => {
+        humanplayer1.classList.add("selected");
+        compplayer1.classList.remove("selected");
+        player1type="Human"
+    });
+
+    humanplayer2.addEventListener('click', () => {
+        humanplayer2.classList.add("selected");
+        compplayer2.classList.remove("selected");
+        player2type="Human";
+    });
+
+    compplayer1.addEventListener('click', () => {
+        compplayer1.classList.add("selected");
+        humanplayer1.classList.remove("selected");
+        player1type="Computer";
+    });
+
+    compplayer2.addEventListener('click', () => {
+        compplayer2.classList.add("selected");
+        humanplayer2.classList.remove("selected");
+        player2type="Computer";
+    });
+
+    const startGame = () => {
+        start.addEventListener('click', () => {
+            if(!player1type || !player2type){
+                alert("select player type");
+                return;
+            }
+
+            const player1=Player(player1type,"X");
+            const player2=Player(player2type,"O");
+
+            assignplayer(player1,player2);
+
+            turntext.classList.remove("hidden");
+            GB.classList.remove("hidden");
+            start.classList.add("hidden");
+            turntext.textContent='${player1.name} turn (${player1.symbol})';
+        })
+    }
 
 })
